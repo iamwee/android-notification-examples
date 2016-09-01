4ab16b672e3af44bf36aa203672d159726618af8
iamwee <wee_finalking@hotmail.com>
2016-09-01 15:05:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/zeon/notificationexamples/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.zeon.notificationexamples;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnShow = (Button) findViewById(R.id.btn_show);
+
+
+        btnShow.setOnClickListener(clickListener);
+    }
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(v == btnShow){
+                startService(new Intent(MainActivity.this, NotificationService.class));
+                Toast.makeText(MainActivity.this, "Notification will show in 5 second", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    };
+}
---
+++ app/src/main/java/com/example/zeon/notificationexamples/NotificationService.java
@@ -0,0 +1,74 @@
+package com.example.zeon.notificationexamples;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+/**
+ * Created by Zeon on 1/9/2559.
+ */
+public class NotificationService extends Service {
+
+    Handler handler;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        handler = new Handler(getMainLooper()){
+            @Override
+            public void handleMessage(Message msg) {
+                pushNotification();
+            }
+        };
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handler.sendEmptyMessageDelayed(1, 5000);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        handler.removeMessages(1);
+    }
+
+    private void pushNotification() {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        mBuilder.setContentIntent(resultPendingIntent);
+        int mNotificationId = 001;
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+        Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_LONG).show();
+    }
+}
